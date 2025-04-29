11006fd780ff5ecc78c751bc61a257415557aa59
Baljan Kanatbek <baljann19@gmail.com>
2025-04-29 08:25:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+async function getCurrencies() {
+  const url = "https://www.cbr-xml-daily.ru/daily_json.js";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const usdRate = data.Valute.USD.Value.toFixed(2);
+  const eurRate = data.Valute.EUR.Value.toFixed(2);
+
+  const usdElement = document.querySelector("#usd");
+  const eurElement = document.querySelector("#eur");
+
+  usdElement.innerText = usdRate;
+  eurElement.innerText = eurRate;
+}
+
+getCurrencies();
